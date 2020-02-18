b80387fceb9c9a88370a3e1c92615dacbde4c41a
Michelle Cupp <shellcupp@gmail.com>
2020-02-18 14:36:24 -0600

    added store

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+const store = createStore();
+//console.log(store.getState());
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+    <Provider>
+    <App />
+    </Provider>, 
+    rootElement);
